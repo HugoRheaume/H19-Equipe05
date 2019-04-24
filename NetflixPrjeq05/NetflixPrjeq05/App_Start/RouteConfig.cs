aef817ebcf2687de24c0d720b7c38f843828b9a4
1681226 <1681226@LLBD0603INFO15>
2019-04-24 15:57:51 -0400

    RouteConfig to Contenus/Contenu

--- NetflixPrjeq05/NetflixPrjeq05/App_Start/RouteConfig.cs
+++ NetflixPrjeq05/NetflixPrjeq05/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NetflixPrjeq05
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Contenus", action = "Contenu", id = UrlParameter.Optional }
             );
         }
     }
