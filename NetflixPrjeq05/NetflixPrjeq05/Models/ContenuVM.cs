62fd66f31f9001d4106dc5a2143686b0ea469ff9
1681226 <1681226@LLBD0603INFO15>
2019-04-24 14:34:19 -0400

    ContenuVM

--- NetflixPrjeq05/NetflixPrjeq05/Models/ContenuVM.cs
+++ NetflixPrjeq05/NetflixPrjeq05/Models/ContenuVM.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,44 @@ namespace NetflixPrjeq05.Models
 {
     public class ContenuVM
     {
+        public ContenuVM()
+        {
+        }
+
+        public ContenuVM(Contenu contenu)
+        {                
+            ContenuId = contenu.ContenuId;                        
+            DateSortie = (DateTime)contenu.Date_de_sortie;           
+            Duree = (int)contenu.Duree;           
+            Titre = contenu.Titre_Original;
+        }
+
+
+        public int ContenuId { get; set; }
+        
+        public string Titre { get; set; }
+
+        public string Status { get; set; }
+
+        public string Affiche { get; set; }
+
+        public int Budget { get; set; }
+
+        public decimal CoteMoyenne { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayName("Date de sortie")]
+        public DateTime DateSortie { get; set; }
+
+        public string Description { get; set; }
+
+        public int Duree { get; set; }
+    
+        public int NbCotes { get; set; }
+                     
+        public string Doublages { get; set; }
+
+        public string Origines { get; set; }
 
     }
 }
