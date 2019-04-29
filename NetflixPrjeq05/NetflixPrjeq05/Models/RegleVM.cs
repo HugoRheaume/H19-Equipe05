b8dada2c365fbb07832630017525ffde8c071327
1634721 <1634721@LLBD0605INFO11>
2019-04-29 15:57:15 -0400

    Suppression des action inutiles

---
+++ NetflixPrjeq05/NetflixPrjeq05/Models/RegleVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetflixPrjeq05.Models
+{
+    public class RegleVM
+    {
+        public RegleVM()
+        {
+
+        }
+        public RegleVM(Regle regle)
+        {
+
+        }
+        public int RegleID; 
+    }
+}
